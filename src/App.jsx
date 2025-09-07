ab5db115b70b655314d8369cd76fc2f181ea390d
Umair Amjad <umairamjad970@gmail.com>
2025-09-06 20:49:53 -0700

    update navabar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-  import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-  import Home from "./pages/Home";
-  import NotFound from "./pages/NotFound";
+  // import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+  // import Home from "./pages/Home";
+  // import NotFound from "./pages/NotFound";
 
-  function App() {
-    return (
-      <>
-        <HashRouter>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </HashRouter>
-      </>
-    );
-  }
+  // function App() {
+  //   return (
+  //     <>
+  //       <HashRouter>
+  //         <Routes>
+  //           {/* <Route index element={<Home />} /> */}
+  //           <Route path="/" element={<Home />} />
+  //           <Route path="*" element={<NotFound />} />
+  //         </Routes>
+  //       </HashRouter>
+  //     </>
+  //   );
+  // }
 
-  export default App;
+  // export default App;
 
+import Home from "./pages/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
