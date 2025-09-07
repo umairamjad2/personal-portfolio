12a201e45a2e31471bc6dae3263de06f7474ef6c
Umair Amjad <umairamjad970@gmail.com>
2025-09-06 20:03:22 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-  import { BrowserRouter, Route, Routes } from "react-router-dom";
+  import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
   import Home from "./pages/Home";
   import NotFound from "./pages/NotFound";
 
   function App() {
     return (
       <>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route index element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </>
     );
   }
