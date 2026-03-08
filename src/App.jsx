a84dec8534a1398fb150f3f88cdba0075afde110
Umair Amjad <umairamjad970@gmail.com>
2026-03-08 14:11:34 +0530

    Clean up App.jsx by removing unused imports and code
    Removed commented-out code for routing and components.

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,4 @@ function App() {
 export default App;
 
 
-  // import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-  // import Home from "./pages/Home";
-  // import NotFound from "./pages/NotFound";
-
-  // function App() {
-  //   return (
-  //     <>
-  //       <HashRouter>
-  //         <Routes>
-  //           {/* <Route index element={<Home />} /> */}
-  //           <Route path="/" element={<Home />} />
-  //           <Route path="*" element={<NotFound />} />
-  //         </Routes>
-  //       </HashRouter>
-  //     </>
-  //   );
-  // }
-
-  // export default App;
+ 
